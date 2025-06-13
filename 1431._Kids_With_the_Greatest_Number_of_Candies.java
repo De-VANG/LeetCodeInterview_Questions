2d33a56d56720287dc3fed62ccbf04830e887c75
De-VANG <devangbhatnagar12@gmail.com>
2025-06-13 19:13:44 +0530

    Day 2

---
+++ 1431._Kids_With_the_Greatest_Number_of_Candies.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int cadmax = Integer.MIN_VALUE;
+        for(int i= 0; i < candies.length; i++){
+            if(candies[i]> cadmax){
+                cadmax = candies[i];
+            }
+        }
+        List<Boolean> result = new ArrayList<>();
+        for (int i = 0; i< candies.length; i++){
+            if(candies[i]+ extraCandies >= cadmax){
+                result.add(true);
+            }
+            else{
+                result.add(false);
+            }
+        }
+        return result;
+    }
+}
