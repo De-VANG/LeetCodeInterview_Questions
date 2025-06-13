9d8bfc5d2c8171897a96ab3a0d281ff1451f0829
De-VANG <devangbhatnagar12@gmail.com>
2025-06-13 20:03:47 +0530

    Day 2

--- 1431._Kids_With_the_Greatest_Number_of_Candies.java
+++ 1431._Kids_With_the_Greatest_Number_of_Candies.java
@@ -1,3 +1,7 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
     public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
         int cadmax = Integer.MIN_VALUE;
